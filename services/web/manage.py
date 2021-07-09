66d28befa47d3f363e6aa09e4d5c162ff1db7148
Svetlana <svetasumets@gmail.com>
2021-07-09 19:31:32 +0300

    Commit Docker settings

---
+++ services/web/manage.py
@@ -0,0 +1,22 @@
+"""
+Management file
+"""
+from flask.cli import FlaskGroup
+from project import db, app
+
+
+cli = FlaskGroup(app)
+
+
+@cli.command("create_db")
+def create_db() -> None:
+    """
+    Database creation function
+    """
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    cli()
